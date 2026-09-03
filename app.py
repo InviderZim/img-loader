9295ebd339068b4c3e839e6d026b6c7b8a0e91b6
Dmytro B. <reg.dmitry.bo@gmail.com>
2026-09-03 21:03:15 +0300

    Add basic POST upload response

--- app.py
+++ app.py
@@ -1,1 +1,57 @@
-print("Hello, Full Stack!")
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+with open("static/index.html", "r", encoding="utf-8") as file:
+    html = file.read()
+
+with open("static/upload.html", "r", encoding="utf-8") as file:
+    upload = file.read()
+
+with open("static/images.html", "r", encoding="utf-8") as file:
+    images = file.read()
+
+
+class ImageServerHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.end_headers()
+
+            self.send_html(html)
+
+        elif self.path == "/upload":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.end_headers()
+
+            self.send_html(upload)
+
+        elif self.path == "/images/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.end_headers()
+
+            self.send_html(images)
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html; charset=utf-8")
+            self.end_headers()
+            self.wfile.write("Помилка 404: Сторінка не знайдена".encode())
+
+    def send_html(self, html):
+        return self.wfile.write(html.encode())
+
+    def do_POST(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html; charset=utf-8")
+        self.end_headers()
+        self.wfile.write("Готово".encode())
+        print(self.headers["Content-Type"])
+        print(self.headers["Content-Length"])
+
+
+if __name__ == "__main__":
+    server = HTTPServer(("0.0.0.0", 8000), ImageServerHandler)
+    print("Server running on http://localhost:8000")
+    server.serve_forever()
